1138d6596dbb88809c9cf7f796f4fd01205b72ad
ElenaIva <ei492493@gmail.com>
2024-09-02 13:37:27 +0200

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,11 @@
+tasks_quantity = "12"
+print(tasks_quantity)
+hours_quantity = "1.5"
+print(hours_quantity)
+course_name = "Python"
+print(course_name)
+one_task_time = "0.125"
+print(one_task_time)
+print("Курс:", course_name, "всего задач:", tasks_quantity, "звтрачено часов:", hours_quantity,  "среднее время выполнения:", one_task_time, "часов")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
